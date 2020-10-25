c43306c81e32d3b1223d40827deed99155db18ed
Hilger Leon <sirit20@gmail.com>
2020-10-25 03:50:03 -0400

    Primer Commit

---
+++ client/src/components/Register.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <h1>Registro</h1>
+    <div class='row justify-content-center'>
+      <div class='col-sm-4'>
+        <label>Email</label>
+        <input type="email" id="email" placeholder="Correo" class="form-control" v-model="email" />
+      </div>
+    </div>
+    <div class='row justify-content-center mt-2'>
+      <div class='col-sm-4'>
+        <label>Password</label>
+        <input type="password" id="password" placeholder="Clave" class="form-control" v-model="password" />
+      </div>
+    </div>
+    <div class='row justify-content-center mt-2'>
+      <div class='col-sm-4'>
+        <button class="btn btn-success" @click="register">
+          Registrar
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      email: 'abcx',
+      password: '123'
+    }
+  },
+  methods: {
+    async register () {
+      const response = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(response.data)
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email cambiado', value)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ client/src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Register from '@/components/Register'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(Router)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    }
+  ]
+})
